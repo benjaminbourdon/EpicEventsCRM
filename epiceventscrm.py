af1c9c61198b39dd4202123add226d75704ca6d2
Benjamin B <dev@benjamin-b.fr>
2023-10-11 18:37:33 +0200

    Upgrade sentry config initialisation

--- epiceventscrm.py
+++ epiceventscrm.py
@@ -1,6 +1,7 @@
 import click
 import os
 import sentry_sdk
+from sentry_sdk.integrations.atexit import AtexitIntegration
 from dotenv import load_dotenv
 
 from controllers import auth, client, contract, employee, event
@@ -9,7 +10,7 @@ load_dotenv()
 SENTRY_DSN = os.getenv("SENTRY_DSN")
 TRACES_SAMPLE_RATE = float(os.getenv("TRACES_SAMPLE_RATE"))
 PROFILES_SAMPLE_RATE = float(os.getenv("PROFILES_SAMPLE_RATE"))
-
+DEBUG_MODE = os.getenv("DEBUG_MODE").lower() in ("1", "true")
 
 cli = click.CommandCollection(
     sources=[
@@ -18,14 +19,23 @@ cli = click.CommandCollection(
         client.client_group,
         contract.contract_group,
         event.event_group,
-    ]
+    ],
 )
 
+
+def atexit_callback(pending, timout):
+    """Method which do nothing to overide default callback.
+    Default method write in stderr and interfere with cli actions."""
+    pass
+
+
 if __name__ == "__main__":
-    # Initializing Sentry
-    sentry_sdk.init(
-        dsn=SENTRY_DSN,
-        traces_sample_rate=TRACES_SAMPLE_RATE,
-        profiles_sample_rate=PROFILES_SAMPLE_RATE,
-    )
+    if not DEBUG_MODE:
+        sentry_sdk.init(
+            dsn=SENTRY_DSN,
+            traces_sample_rate=TRACES_SAMPLE_RATE,
+            profiles_sample_rate=PROFILES_SAMPLE_RATE,
+            debug=DEBUG_MODE,
+            integrations=[AtexitIntegration(callback=atexit_callback)],
+        )
     cli()
