2d5813d5961bcdfc606d49b189f92027a66f200c
Benjamin B <dev@benjamin-b.fr>
2023-10-17 12:35:37 +0200

    Simplify getting db session methods

--- db.py
+++ db.py
@@ -1,6 +1,5 @@
 import os
 
-import click
 from dotenv import load_dotenv
 from sqlalchemy import URL, create_engine
 from sqlalchemy.orm import sessionmaker
@@ -8,17 +7,18 @@ from sqlalchemy.orm import sessionmaker
 load_dotenv()
 
 DATEBASE_NAME = os.getenv("DATEBASE_NAME")
-ADMIN_USERNAME = os.getenv("ADMIN_USERNAME")
-ADMIN_PASSWORD = os.getenv("ADMIN_PASSWORD")
+DB_USERNAME = os.getenv("DB_USERNAME")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
 DEBUG_MODE = os.getenv("DEBUG_MODE").lower() in ("1", "true")
+DB_HOST = os.getenv("DB_HOST")
 
 
 def get_db_url():
     return URL.create(
         drivername="postgresql+psycopg2",
-        username=ADMIN_USERNAME,
-        password=ADMIN_PASSWORD,
-        host="localhost",
+        username=DB_USERNAME,
+        password=DB_PASSWORD,
+        host=DB_HOST,
         database=DATEBASE_NAME,
     )
 
@@ -28,50 +28,11 @@ def get_admin_engine():
 
     return create_engine(
         url_object,
-        # "postgresql+psycopg2://postgres:OC-2023@localhost/test",  # Superuser
         echo=DEBUG_MODE,
     )
 
 
-def get_admin_session():
+def get_session():
     engine = get_admin_engine()
 
     return sessionmaker(engine)
-
-
-def get_session(role=None):
-    session = get_admin_session()
-
-    # SET ROLE 'name_role'
-    # if role == RoleEmployees.commercial:
-    #     pass
-    # elif role == RoleEmployees.support:
-    #     pass
-    # elif role == RoleEmployees.support:
-    #     pass
-    # else:
-    #     set role select on employee
-
-    return session
-
-
-@click.group()
-def index():
-    pass
-
-
-@index.command()
-def create_tables():
-    # Ajouter mdp au superuser (prompt)
-    # Creer compte admin
-    # Creer roles support, commercial et gestion
-    # Ajouter les role au role admin
-
-    import models
-
-    engine = get_admin_engine()
-    models.Base.metadata.create_all(engine)
-
-
-if __name__ == "__main__":
-    index()
